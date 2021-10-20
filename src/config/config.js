c436dcc5b2e615addb89eaf8c3088500b6069f82
mouren <1832819123@qq.com>
2021-10-20 11:29:11 +0800

    Update config.js

--- src/config/config.js
+++ src/config/config.js
@@ -5,5 +5,6 @@ config({
   "discovery.wallet": process.env.REACT_APP_WALLET_DISCOVERY,
   "0xFungibleToken": process.env.REACT_APP_FT_CONTRACT,
   "0xFlowToken": process.env.REACT_APP_FUSD_CONTRACT,
-  "0xATTANFT": process.env.REACT_APP_DAPPY_CONTRACT
+  "0xATTANFT": process.env.REACT_APP_DAPPY_CONTRACT,
+  "0xNonFungibleToken": process.env.FLOW_NONFUNGIBLE_ADDRESS
 })
