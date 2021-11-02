c5facf53c61c662babe74017da0b69b28c0f037d
wangjinyi <wangjinyi143>
2021-11-02 16:23:08 +0800

    11.2-2

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,8 +1,8 @@
-import React,{ useState} from 'react'
+import React,{ useState,useEffect} from 'react'
 import useCurrentUser from '../hooks/use-current-user.hook'
 import { useHistory } from "react-router-dom"
 import { useUser } from '../providers/UserProvider'
-import {setCookie} from '../utils/utils'
+import {setCookie,getCookie} from '../utils/utils'
 
 import "./Navbar.css"
 
@@ -11,8 +11,29 @@ export default function Navbar() {
   const [user, loggedIn, tools] = useCurrentUser()
   const { balance, createFUSDVault } = useUser()
   const { collection, createCollection } = useUser()
+  const [languageType,setLanguageType] = useState('TC');
+  const [chEnTextHtml] = useState({
+		"TC": {
+			home:"首頁",
+      auction:"拍賣",
+      COLLECTIONS:"明星藏品",
+      specialTool:"無限制電競大會",
+      blindbox:"盲盒",
+		},
+		"EN": {
+			home:"HOME",
+      auction:"AUCTION",
+      COLLECTIONS:"COLLECTIONS",
+      specialTool:"INFINITY ESPORTS TOURNAMENT",
+      blindbox:"MYSTERY BOX",
+		}
+	})
 
   const [modalIsOpen,setModalIsOpen] = useState('none');
+
+  useEffect (()=>{
+		setLanguageType(getCookie("lang")?getCookie("lang"):'TC');
+	},[])
 
   const handleMouseOver = (e) => {
     setModalIsOpen('block')
@@ -34,19 +55,19 @@ export default function Navbar() {
           <a className="header-left-logo" target="_blank" href="https://www.bazhuayu.io/mobile/tc/index.html" rel="noreferrer"><img src="https://www.bazhuayu.io/mobile/tc/images/Brand.png" alt='' /></a>
           <ul className="nav-header flex">
             <li>
-              <a className="language-tc" target="_blank" href="https://www.bazhuayu.io/mobile/tc/index.html" rel="noreferrer">首頁</a>
+              <a className="language-tc" target="_blank" href="https://www.bazhuayu.io/mobile/tc/index.html" rel="noreferrer">{chEnTextHtml[languageType].home}</a>
             </li>
             <li>
-              <a className="language-tc" target="_blank" href="https://www.bazhuayu.io/mobile/tc/blindbox.html" rel="noreferrer">盲盒</a>
+              <a className="language-tc" target="_blank" href="https://www.bazhuayu.io/mobile/tc/blindbox.html" rel="noreferrer">{chEnTextHtml[languageType].blindbox}</a>
             </li>
             <li>
-              <a className="language-tc" target="_blank" href="https://www.bazhuayu.io/mobile/tc/artwork.html?id=1" rel="noreferrer">明星藏品</a>
+              <a className="language-tc" target="_blank" href="https://www.bazhuayu.io/mobile/tc/artwork.html?id=1" rel="noreferrer">{chEnTextHtml[languageType].COLLECTIONS}</a>
             </li>
             <li>
-              <a className="language-tc" target="_blank" href="https://www.bazhuayu.io/mobile/tc/auctionDetails.html?id=5" rel="noreferrer">拍賣</a>
+              <a className="language-tc" target="_blank" href="https://www.bazhuayu.io/mobile/tc/auctionDetails.html?id=5" rel="noreferrer">{chEnTextHtml[languageType].auction}</a>
             </li>
             <li>
-              <a className="language-tc" target="_blank" href="https://www.bazhuayu.io/mobile/tc/specialitem.html" rel="noreferrer">無限制電競大會</a>
+              <a className="language-tc" target="_blank" href="https://www.bazhuayu.io/mobile/tc/specialitem.html" rel="noreferrer">{chEnTextHtml[languageType].specialTool}</a>
             </li>
             <li className="current" onMouseOver={handleMouseOver}
               onMouseLeave={handleMouseOut}>
