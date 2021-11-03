860c89299e4d18fdbafd5d74991b8f367b4b8f6f
mouren <1832819123@qq.com>
2021-11-03 14:03:34 +0800

    修改背景色和登陆

--- src/providers/AuthProvider.js
+++ src/providers/AuthProvider.js
@@ -10,12 +10,9 @@ export default function AuthProvider({ children }) {
 
   if (!user || !loggedIn) return (
     <div className="app">
-      <Header
-        title={<><span className="highlight">Crypto</span>Dappy</>}
-        subtitle={<>The brand new <span className="highlight">collectible game</span> on the blockchain</>}
-      />
+      <Header/>
       <div
-        style={{ display: "inline-block" }}
+        style={{ display: "inline-block",marginTop: '100px' }}
         className="btn btn-bg rounded"
         onClick={() => tools?.logIn()}>
         Sign In to start
