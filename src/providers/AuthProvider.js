70347801bc6544220c547013a8654989bdc7e276
zhanghaiqing <zhanghaiqing>
2021-12-01 10:41:23 +0800

    12.1-3

--- src/providers/AuthProvider.js
+++ src/providers/AuthProvider.js
@@ -1,13 +1,19 @@
-import React, { createContext, useContext } from 'react'
+import React, { createContext, useContext} from 'react'
 
 import useCurrentUser from '../hooks/use-current-user.hook'
 import Header from '../components/Header'
+import { getCookie } from '../utils/utils'
 
 const AuthContext = createContext()
 
 export default function AuthProvider({ children }) {
-  const [user, loggedIn, tools] = useCurrentUser()
-
+  const [user, loggedIn, tools] = useCurrentUser();
+  const lang = getCookie("lang") || 'TC';
+  const stylecss = {
+    marginTop: '20px',
+    fontSize: '14px',
+    color: '#ccc'
+  }
   if (!user || !loggedIn) return (
     <div className="app">
       <Header/>
@@ -15,8 +21,14 @@ export default function AuthProvider({ children }) {
         style={{ display: "inline-block",marginTop: '100px' }}
         className="btn btn-bg rounded"
         onClick={() => tools?.logIn()}>
-        Sign In to start
+        Sign in with Blocto
       </div>
+      {
+        lang == "TC" ?
+        <p style={stylecss}>对Blocto钱包不熟悉？点击 <a style={stylecss} href='https://bbzx2018.feishu.cn/docs/doccnWw7iPRq3JxQYNbaEhUDhUf'>URL</a> 了解。</p>
+        :
+        <p style={stylecss}>Unfamiliar with Blocto? Click <a style={stylecss} href='https://bbzx2018.feishu.cn/docs/doccnWw7iPRq3JxQYNbaEhUDhUf'>here</a> to know more.</p>
+      }
     </div>
   )
 
